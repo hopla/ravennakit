2e832c72c10d71e840a0a2817cb8dd59f7477a29
Ruurd Adema <ruurd@owllab.nl>
2024-07-02 21:50:26 +0200

    Setup basic project

--- .idea/fileTemplates/includes/C File Header.h
+++ .idea/fileTemplates/includes/C File Header.h
@@ -1,3 +1,4 @@
+#if ($HEADER_COMMENTS)
 /*
  * Owllab License Agreement
  *
@@ -5,13 +6,7 @@
  * merged, published, distributed, sublicensed, or sold without a valid and
  * explicit agreement with Owllab.
  *
- * Copyright (c) 2024 Owllab. All rights reserved.
+ * Copyright (c) $YEAR Owllab. All rights reserved.
  */
 
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
-
+#end

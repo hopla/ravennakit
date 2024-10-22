7b525dbabf455bd45ace85728de863359310cf2b
Ruurd Adema <ruurd@owllab.nl>
2024-10-22 22:44:59 +0200

    Add byte_stream class

---
+++ include/ravennakit/containers/byte_stream.hpp
@@ -0,0 +1,47 @@
+/*
+ * Owllab License Agreement
+ *
+ * This software is provided by Owllab and may not be used, copied, modified,
+ * merged, published, distributed, sublicensed, or sold without a valid and
+ * explicit agreement with Owllab.
+ *
+ * Copyright (c) 2024 Owllab. All rights reserved.
+ */
+
+#pragma once
+
+#include "input_stream.hpp"
+#include "output_stream.hpp"
+
+#include <vector>
+
+namespace rav {
+
+/**
+ * Simple stream implementation that writes to and reads from a vector.
+ */
+class byte_stream final: public input_stream, public output_stream {
+  public:
+    byte_stream() = default;
+    explicit byte_stream(std::vector<uint8_t> data);
+
+    // input_stream overrides
+    size_t read(uint8_t* buffer, size_t size) override;
+    bool set_read_position(size_t position) override;
+    [[nodiscard]] size_t get_read_position() override;
+    [[nodiscard]] std::optional<size_t> size() const override;
+    [[nodiscard]] bool exhausted() const override;
+
+    // output_stream overrides
+    size_t write(const uint8_t* buffer, size_t size) override;
+    bool set_write_position(size_t position) override;
+    [[nodiscard]] size_t get_write_position() const override;
+    void flush() override;
+
+  private:
+    std::vector<uint8_t> data_;
+    size_t read_position_ = 0;
+    size_t write_position_ = 0;
+};
+
+}  // namespace rav
---
+++ src/ravennakit/containers/byte_stream.cpp
@@ -0,0 +1,68 @@
+/*
+ * Owllab License Agreement
+ *
+ * This software is provided by Owllab and may not be used, copied, modified,
+ * merged, published, distributed, sublicensed, or sold without a valid and
+ * explicit agreement with Owllab.
+ *
+ * Copyright (c) 2024 Owllab. All rights reserved.
+ */
+
+#include "ravennakit/containers/byte_stream.hpp"
+
+rav::byte_stream::byte_stream(std::vector<uint8_t> data) : data_(std::move(data)), write_position_(data_.size()) {}
+
+size_t rav::byte_stream::read(uint8_t* buffer, const size_t size) {
+    if (exhausted()) {
+        return 0;
+    }
+
+    const size_t bytes_to_read = std::min(size, data_.size() - read_position_);
+    std::memcpy(buffer, data_.data() + read_position_, bytes_to_read);
+    read_position_ += bytes_to_read;
+    return bytes_to_read;
+}
+
+bool rav::byte_stream::set_read_position(const size_t position) {
+    if (position > data_.size()) {
+        return false;
+    }
+    read_position_ = position;
+    return true;
+}
+
+size_t rav::byte_stream::get_read_position() {
+    return read_position_;
+}
+
+std::optional<size_t> rav::byte_stream::size() const {
+    return data_.size();
+}
+
+bool rav::byte_stream::exhausted() const {
+    return read_position_ >= data_.size() || read_position_ == write_position_;
+}
+
+size_t rav::byte_stream::write(const uint8_t* buffer, const size_t size) {
+    if (write_position_ + size > data_.size()) {
+        data_.resize(write_position_ + size);
+    }
+
+    std::memcpy(data_.data() + write_position_, buffer, size);
+    write_position_ += size;
+    return size;
+}
+
+bool rav::byte_stream::set_write_position(const size_t position) {
+    if (position > data_.size()) {
+        return false;
+    }
+    write_position_ = position;
+    return true;
+}
+
+size_t rav::byte_stream::get_write_position() const {
+    return write_position_;
+}
+
+void rav::byte_stream::flush() {}

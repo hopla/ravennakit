a3a02a3480ea51ed55374285e11643df0a4bf2db
Ruurd Adema <ruurd@owllab.nl>
2025-01-01 16:24:28 +0100

    Use get_random_int inside get_random_interval (RAV-36)

--- include/ravennakit/core/random.hpp
+++ include/ravennakit/core/random.hpp
@@ -56,8 +56,7 @@ class random {
      */
     template<typename T = int>
     std::chrono::milliseconds get_random_interval(T min_ms, T max_ms) {
-        std::uniform_int_distribution<T> dist(min_ms, max_ms);
-        return std::chrono::milliseconds(dist(generator_));
+        return std::chrono::milliseconds(get_random_int<T>(min_ms, max_ms));
     }
 
   private:

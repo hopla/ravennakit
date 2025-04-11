933213cb58d95215439c87715b213d55469a7b71
Ruurd Adema <ruurd@owllab.nl>
2025-04-11 09:12:11 +0200

    Include cstring (RAV-94)

--- include/ravennakit/core/audio/audio_encoding.hpp
+++ include/ravennakit/core/audio/audio_encoding.hpp
@@ -12,6 +12,7 @@
 
 #include <cstdint>
 #include <optional>
+#include <cstring>
 
 namespace rav {
 
@@ -95,28 +96,28 @@ inline std::optional<AudioEncoding> audio_encoding_from_string(const char* str) 
     if (!str) {
         return std::nullopt;
     }
-    if (strcmp(str, "pcm_s8") == 0) {
+    if (std::strcmp(str, "pcm_s8") == 0) {
         return AudioEncoding::pcm_s8;
     }
-    if (strcmp(str, "pcm_u8") == 0) {
+    if (std::strcmp(str, "pcm_u8") == 0) {
         return AudioEncoding::pcm_u8;
     }
-    if (strcmp(str, "pcm_s16") == 0) {
+    if (std::strcmp(str, "pcm_s16") == 0) {
         return AudioEncoding::pcm_s16;
     }
-    if (strcmp(str, "pcm_s24") == 0) {
+    if (std::strcmp(str, "pcm_s24") == 0) {
         return AudioEncoding::pcm_s24;
     }
-    if (strcmp(str, "pcm_s32") == 0) {
+    if (std::strcmp(str, "pcm_s32") == 0) {
         return AudioEncoding::pcm_s32;
     }
-    if (strcmp(str, "pcm_f32") == 0) {
+    if (std::strcmp(str, "pcm_f32") == 0) {
         return AudioEncoding::pcm_f32;
     }
-    if (strcmp(str, "pcm_f64") == 0) {
+    if (std::strcmp(str, "pcm_f64") == 0) {
         return AudioEncoding::pcm_f64;
     }
-    if (strcmp(str, "undefined") == 0) {
+    if (std::strcmp(str, "undefined") == 0) {
         return AudioEncoding::undefined;
     }
     return std::nullopt;
